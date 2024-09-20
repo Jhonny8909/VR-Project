7aa8d46e38be6c39d7ad9e42467686b092ed4143
Jonathan Pacheco <cdmx2508@amerike.edu.mx>
2024-09-20 13:19:04 -0600

    ya se mueve el enemigo, solo debo de checar las distancias para la deteccion y las velocidades

--- Assets/Scenes/Jonathan/Script/AIController.cs
+++ Assets/Scenes/Jonathan/Script/AIController.cs
@@ -17,25 +17,13 @@ public class AIController : MonoBehaviour
     public float chaseRange = 2f;           
     public float stopDistance = 1.5f;
 
-    private NavMeshAgent agent;             
-    private Rigidbody rb;                   
+    private NavMeshAgent agent;                             
     private bool playerDetected = false;
-
-
-    private void Start()
-    {
-        agent = GetComponent<NavMeshAgent>();
-        rb = GetComponent<Rigidbody>();
-
-        agent.SetDestination(player.transform.position);
-    }
-
-    /*void Start()
+    void Start()
     {
         currentState = EnemyState.Idle;
 
         agent = GetComponent<NavMeshAgent>();
-        rb = GetComponent<Rigidbody>();
 
         agent.updatePosition = false;
         agent.updateRotation = true;
@@ -60,6 +48,7 @@ public class AIController : MonoBehaviour
     void Idle()
     {
         Debug.Log("Enemy is idle.");
+        
         if (!playerDetected && Vector3.Distance(transform.position, player.position) <= detectionRange)
         {
             playerDetected = true;
@@ -77,9 +66,9 @@ public class AIController : MonoBehaviour
         }
         else
         {
+            agent.updatePosition = true;
             agent.SetDestination(player.position);
-
-            MoveAgent();
+            agent.speed = 1;
         }
 
         if (Vector3.Distance(transform.position, player.position) > detectionRange)
@@ -95,9 +84,9 @@ public class AIController : MonoBehaviour
 
         if (Vector3.Distance(transform.position, player.position) <= chaseRange)
         {
+            agent.updatePosition = true;
             agent.SetDestination(player.position);
-
-            MoveAgent();
+            agent.speed = 3;
         }
 
         if (Vector3.Distance(transform.position, player.position) > detectionRange)
@@ -106,19 +95,6 @@ public class AIController : MonoBehaviour
             currentState = EnemyState.Idle;
         }
     }
-
-    void MoveAgent()
-    {
-        if (agent.remainingDistance > agent.stoppingDistance)
-        {
-            Vector3 velocity = player.transform.position;
-            agent.SetDestination(velocity);
-        }
-        else
-        {
-            rb.velocity = Vector3.zero;  
-        }
-    }*/
 
     private void OnDrawGizmosSelected()
     {
