4bc9f79ae1868138f7dbbc05bc432e2600cd4eba
Grecia Lorena Morales Castillo <cdmx2510@amerike.edu.mx>
2024-09-24 23:49:09 -0600

    Invisibilidad usando Raycast
    Enemigos detectan al player usando Raycast, identificando obstaculos y la accion de invisibilidad, la cual tiene cooldown y el shader del jugador, se activa presionando el gatillo del VR controller o la tecla E.

---
+++ Assets/Scripts/VRInvisibility.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit; 
+using System.Collections;
+using UnityEngine.XR; 
+
+public class VRInvisibility : MonoBehaviour
+{
+    public float invisibilityDuration = 5f; 
+    public float cooldownDuration = 15f;    
+    public bool IsInvisible { get; private set; }  
+    private bool isInCooldown = false; 
+
+    public InputActionProperty vrButtonAction;  
+    public Material invisibleMaterial; 
+    public Material originalMaterial; 
+    private Renderer playerRenderer;   
+
+    private XRController controllerDevice; 
+
+    void Start()
+    {
+        playerRenderer = GetComponent<Renderer>();
+
+        if (playerRenderer != null)
+        {
+            originalMaterial = playerRenderer.material; 
+        }
+
+        controllerDevice = GetComponent<XRController>();
+    }
+
+    void Update()
+    {
+        if (Keyboard.current.eKey.wasPressedThisFrame && !isInCooldown)
+        {
+            ActivateInvisibility();
+        }
+
+        if (controllerDevice != null)
+        {
+            if (controllerDevice.inputDevice.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out bool triggerButtonPressed) && triggerButtonPressed && !isInCooldown)
+            {
+                ActivateInvisibility();
+            }
+        }
+
+        if (vrButtonAction.action != null && vrButtonAction.action.WasPressedThisFrame() && !isInCooldown)
+        {
+            ActivateInvisibility();
+        }
+    }
+
+    void ActivateInvisibility()
+    {
+        if (IsInvisible || isInCooldown) return;
+
+        StartCoroutine(InvisibilityRoutine());
+    }
+
+    private IEnumerator InvisibilityRoutine()
+    {
+        IsInvisible = true;
+        Debug.Log("El jugador es invisible.");
+
+        if (playerRenderer != null)
+        {
+            playerRenderer.material = invisibleMaterial;
+        }
+
+        yield return new WaitForSeconds(invisibilityDuration);
+
+        IsInvisible = false;
+        Debug.Log("El jugador ya no es invisible.");
+
+        if (playerRenderer != null)
+        {
+            playerRenderer.material = originalMaterial;
+        }
+
+        StartCoroutine(CooldownRoutine());
+    }
+
+    private IEnumerator CooldownRoutine()
+    {
+        isInCooldown = true;
+        Debug.Log("Cooldown iniciado.");
+        yield return new WaitForSeconds(cooldownDuration);
+        isInCooldown = false;
+        Debug.Log("Cooldown terminado.");
+    }
+}
