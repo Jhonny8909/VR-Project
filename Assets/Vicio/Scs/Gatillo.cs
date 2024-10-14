201048951b75347ecdd3f543b34d85abee859868
s <cdmx2508@amerike.edu.mx>
2024-10-14 16:00:32 -0600

    gatillo

--- Assets/Vicio/Scs/Gatillo.cs
+++ Assets/Vicio/Scs/Gatillo.cs
@@ -47,7 +47,10 @@ public class Gatillo : MonoBehaviour
                     
                     guide.enabled = true;
                     guide.SetPosition(0, player.transform.position + Vector3.up/2);
-                    guide.SetPosition(1, hasHit ? hit.point : head.transform.position + hit.transform.position);
+                    if (hit != null)
+                    {
+                        guide.SetPosition(1, hasHit ? hit.point : head.transform.position + hit.transform.position);
+                    }
                     
                     Vector3 ubi = new Vector3();
 
